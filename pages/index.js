a3a93f7030cc008e814e235f39fa5b44f8ea86a4
Jesse Egbosionu <j3bsie@gmail.com>
2021-04-30 16:33:39 +0100

    Feat - Add footer and other updates

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,9 @@ export default function Home() {
       </Head>
       <header className="header">
         <nav className="header__navbar">
-          <p className="header__logo">__init__</p>
+          <p className="header__logo">
+            <a href="https://twitter.com/jesseinit">__init__</a>
+          </p>
           <ul className="header__nav">
             {/* <li className="header__nav__item">blog</li>
             <li className="header__nav__item">resume</li> */}
@@ -19,10 +21,30 @@ export default function Home() {
       <main>
         <section className="hero">
           <aside className="hero__aside">
-            <h2 className="hero__greeting">Hi I'm Jesse</h2>
-            <p className="hero__description">I'm a software engineer from Lagos, Nigeria</p>
+            <h2 className="hero__author_name">Jesse Egbosionu</h2>
+            <p className="hero__description">
+              I'm a fullstack (backend focused) software engineer from Lagos, Nigeria, still living
+              in Nigeria. The vast majority of my career have been spent understanding and
+              proactively leveraging technology to impact lives positively far beyond the shores of
+              my environment.
+            </p>
+
+            <p className="hero__description">
+              In the last 3 years I've been building services and products in the finance, ecommerce
+              and health space, enriching lifes using technologies like Python(Django, Flask,
+              FastAPI), Javascript(React, Node.js), PostgreSQL, Docker, Redis etc.
+            </p>
+
+            <p className="hero__description">
+              I'm constantly looking for challenging and impactful opportunities so can always shoot
+              me an{' '}
+              <a className="text-underline" href="mailto:jesse.egbosionu@gmail.com">
+                email
+              </a>
+              .
+            </p>
           </aside>
-          <aside className="hero__aside">
+          <aside className="hero__aside text-align-center">
             <img
               src="https://res.cloudinary.com/dqfjoblug/image/upload/v1619192037/site__hero_rg5gzv.jpg"
               className="hero__image"
@@ -30,6 +52,9 @@ export default function Home() {
           </aside>
         </section>
       </main>
+      <footer className="footer">
+        <p className="text-align-center">Made with Awesomeness and ❤️</p>
+      </footer>
     </div>
   );
 }
